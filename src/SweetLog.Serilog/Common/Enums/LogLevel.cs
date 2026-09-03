ab271e88fa09e5af5261e900bd89a613f9714288
zynres <killp8918@gmail.com>
2026-09-03 23:02:21 +0300

    feat: add new enum log level

---
+++ src/SweetLog.Serilog/Common/Enums/LogLevel.cs
@@ -0,0 +1,11 @@
+namespace SweetLog.Serilog.Common.Enums;
+
+public enum LogLevel : byte
+{
+    Verbose = 0,
+    Debug = 1,
+    Information = 2,
+    Warning = 3,
+    Error = 4,
+    Fatal = 5
+}
