ee685c0d441996984176c3df9b36636915c4cf4f
zynres <killp8918@gmail.com>
2026-09-03 23:02:53 +0300

    feat: add new enum property type

---
+++ src/SweetLog.Serilog/Common/Enums/PropertyType.cs
@@ -0,0 +1,17 @@
+namespace SweetLog.Serilog.Common.Enums;
+
+public enum PropertyType : byte
+{
+    Null = 0,
+    String = 1,
+    Int32 = 2,
+    Int64 = 3,
+    UInt32 = 4,
+    UInt64 = 5,
+    Boolean = 6,
+    Double = 7,
+    Float = 8,
+    Guid = 9,
+    DateTime = 10,
+    Decimal = 11
+}
