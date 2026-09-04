4bba16f756b9e8bc6d44ca074c7d0a5ed4e65fbe
zynres <killp8918@gmail.com>
2026-09-04 22:29:42 +0300

    feat: add empty log sender worker

---
+++ src/SweetLog.Serilog/Workers/LogSenderWorker.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Hosting;
+
+namespace SweetLog.Serilog.Workers;
+
+public class LogSenderWorker : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken token)
+    {
+
+    }
+}
