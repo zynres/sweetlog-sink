bcd2b082ab0422e3b44f651d046fc140de2edc47
zynres <killp8918@gmail.com>
2026-09-08 17:23:10 +0300

    feat: complite the sink extension to use

--- src/SweetLog.Serilog/SinkExtension.cs
+++ src/SweetLog.Serilog/SinkExtension.cs
@@ -1,16 +1,31 @@
 using Microsoft.Extensions.DependencyInjection;
 using SweetLog.Serilog.Workers;
 using SweetLog.Serilog.Buffer;
+using Serilog.Configuration;
+using Serilog;
 
 namespace SweetLog.Serilog;
 
 public static class SinkExtension
 {
-    public static void AddSweetLog(this IServiceCollection services)
+    public static void AddSweetLog(this IServiceCollection services, Action<SinkOptions>? configure = null)
     {
-        services.AddHostedService<LogSenderWorker>();
+        var options = new SinkOptions();
 
-        services.AddSingleton<BatchBuffer>();
+        configure?.Invoke(options);
+
         services.AddSingleton<Sink>();
+        services.AddSingleton(options);
+        services.AddSingleton<BatchBuffer>();
+
+        //services.AddHostedService<HeartbeatWorker>();
+        services.AddHostedService<LogSenderWorker>();
+    }
+
+    public static LoggerConfiguration SweetLog(this LoggerSinkConfiguration sinkConfiguration, IServiceProvider services)
+    {
+        var sink = services.GetRequiredService<Sink>();
+
+        return sinkConfiguration.Sink(sink);
     }
 }
