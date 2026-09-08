041d6654bb41bf4a8f767670333240220de1584e
zynres <killp8918@gmail.com>
2026-09-08 17:25:38 +0300

    style: add comment and rename somefields in sink options

--- src/SweetLog.Serilog/SinkOptions.cs
+++ src/SweetLog.Serilog/SinkOptions.cs
@@ -1,15 +1,15 @@
 namespace SweetLog.Serilog;
 
-public sealed class SinkOptions 
+public sealed class SinkOptions
 {
-    public required Uri Endpoint { get; init; }
-    public required string ApiKey { get; init; }
+    public Uri Uri { get; init; } = new Uri("ws://localhost:5000");
+    public string ApiKey { get; init; } = string.Empty;
 
-    public long MaxDiskBufferSize { get; init; } = 250 * 1024 * 1024;
-    public long MaxRamBufferSize { get; init; } = 150 * 1024 * 1024;
-    
+    public long MaxDiskBufferSize { get; init; } = 1 * 1024 * 1024 * 1024; // 1 Gib
+    public long MaxRamBufferSize { get; init; } = 150 * 1024 * 1024; // 150 Mib
+
     public int QueueCapacity { get; init; } = 96;
-    public int BatchSize { get; init; } = 1 * 1024 * 1024;
+    public int BatchSize { get; init; } = 1 * 1024 * 1024; // 1 Mib
 
     public TimeSpan FlushInterval { get; init; } =
         TimeSpan.FromSeconds(1);
