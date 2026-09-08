8ad429651cd6ab0065d15cc222285c742d1d1f16
zynres <killp8918@gmail.com>
2026-09-08 17:30:50 +0300

    feat: add initial code for the log sender worker

--- src/SweetLog.Serilog/Workers/LogSenderWorker.cs
+++ src/SweetLog.Serilog/Workers/LogSenderWorker.cs
@@ -1,11 +1,40 @@
+using System.Net.WebSockets;
 using Microsoft.Extensions.Hosting;
+using SweetLog.Serilog.Buffer;
 
 namespace SweetLog.Serilog.Workers;
 
 public class LogSenderWorker : BackgroundService
 {
+    private readonly SinkOptions options;
+    private readonly BatchBuffer buffer;
+
+    public LogSenderWorker(BatchBuffer buffer, SinkOptions options)
+    {
+        this.options = options;
+        this.buffer = buffer;
+    }
+
     protected override async Task ExecuteAsync(CancellationToken token)
     {
+        using var client = new ClientWebSocket();
 
+        await client.ConnectAsync(new Uri(options.Uri.AbsoluteUri + "/ws"), token);
+
+        while (!token.IsCancellationRequested)
+        {
+            await Task.Delay(options.FlushInterval);
+
+            ReadOnlyMemory<byte> batch = buffer.Read();
+
+            if (batch.Length == 0)
+                continue;
+
+            await client.SendAsync(
+                batch, WebSocketMessageType.Binary, true, token);
+        }
+
+        await client.CloseAsync(
+            WebSocketCloseStatus.NormalClosure, "Bye", token);
     }
 }
