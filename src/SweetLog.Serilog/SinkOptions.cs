460785953318d05f2342eba63b81e45e6d5c9031
zynres <killp8918@gmail.com>
2026-09-08 17:27:18 +0300

    fix: corrected type from int to uint

--- src/SweetLog.Serilog/SinkOptions.cs
+++ src/SweetLog.Serilog/SinkOptions.cs
@@ -8,8 +8,8 @@ public sealed class SinkOptions
     public long MaxDiskBufferSize { get; init; } = 1 * 1024 * 1024 * 1024; // 1 Gib
     public long MaxRamBufferSize { get; init; } = 150 * 1024 * 1024; // 150 Mib
 
-    public int QueueCapacity { get; init; } = 96;
-    public int BatchSize { get; init; } = 1 * 1024 * 1024; // 1 Mib
+    public uint QueueCapacity { get; init; } = 96;
+    public uint BatchSize { get; init; } = 1 * 1024 * 1024; // 1 Mib
 
     public TimeSpan FlushInterval { get; init; } =
         TimeSpan.FromSeconds(1);
