9d80f824ee2e67251b5b2f886937cfb911f4e4bf
zynres <killp8918@gmail.com>
2026-09-21 23:50:20 +0300

    feat: add heartbeat encoder

--- src/SweetLog.Serilog/Encoders/HeartbeatEncoder.cs
+++ src/SweetLog.Serilog/Encoders/HeartbeatEncoder.cs
@@ -1,16 +1,29 @@
+using SweetLog.Serilog.Common.Enums;
 using SweetLog.Serilog.Common.Data;
+using System.Buffers.Binary;
 
 namespace SweetLog.Serilog.Encoders;
 
-public sealed class HeartbeatEncoder 
-{   
-    public PreparedHeartbeat GetPreparedHeartbeat() 
+public sealed class HeartbeatEncoder
+{
+    public PreparedHeartbeat GetPreparedHeartbeat()
     {
-        return default;
+        return new PreparedHeartbeat()
+        {
+            MessageType = (byte)MessageType.Heartbeat,
+            Timestamp = DateTime.UtcNow.Ticks
+        };
     }
 
     public void Encode(in PreparedHeartbeat prepared, Span<byte> destination, ref int position)
     {
+        destination[position] = prepared.MessageType;
 
+        position++;
+
+        BinaryPrimitives.WriteInt64LittleEndian(
+            destination[position..], prepared.Timestamp);
+
+        position += 8;
     }
 }
